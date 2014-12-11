5063a2ee3b7e6aa3f7562f09a35dda2f520d81b6
sugano-k <sugano-k@gnavi.co.jp>
2014-12-11 13:21:41 +0900

    deletion

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,16 +1,35 @@
 
 class Controller
-  def self.insert(year, month, day, comment)
+  def self.getext(isholiday)
+    if isholiday
+      return "holiday"
+    end
+    return "data"
+  end
+
+  def self.delete(year, month, day)
+    mdir = getMonthDir year, month
+    dataf = "#{mdir}/#{day}.data"
+    holif = "#{mdir}/#{day}.holiday"
+    require 'FileUtils'
+    if File.exist? dataf
+      FileUtils.rm dataf
+    elsif File.exist? holif
+      FileUtils.rm holif
+    end
+  end
+
+  def self.insert(year, month, day, comment, isholiday=false)
     mdir = getMonthDir year, month
     makeDir mdir
-    f = File.open("#{mdir}/#{day}.data", "a")
+    f = File.open("#{mdir}/#{day}.#{getext(isholiday)}", "a")
     f.puts comment
     f.close
   end
 
-  def self.hasSchedule(year, month, day)
+  def self.hasSchedule(year, month, day, isholiday=false)
     has = true
-    file = "%s/%s.data" % [getMonthDir(year, month), day]
+    file = "%s/%s.%s" % [getMonthDir(year, month), day, getext(isholiday)]
     if (!File.exist?(file))
       has = false
     end
